2b01b4925ef0fc80246e89a2e7f10acf174d67bb
Adrian Graça <adrianspk1@gmail.com>
2024-09-18 15:20:13 -0300

    update
    marketing page ok

--- app/components/contatos.tsx
+++ app/components/contatos.tsx
@@ -1,21 +1,57 @@
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { RegisterLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import Link from "next/link";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+import { Facebook, Instagram, MessageCircle, Phone } from "lucide-react";
 
-export default function EmpresaInfo() {
+export default function Contatos() {
     return (
 
-    <section className='border flex items-center justify-center'> 
+    <section className='flex items-center justify-center'> 
         <div className='relative items-center w-full px-5 py-12 mx-auto'>      
             <div className='mx-auto justify-center text-center grid md:grid-cols-2 gap-4'>
       
         {/* lado esquerdo */} 
 
-            <div className='flex justify-center max-w-md mx-auto mt-20 mb-5'>
-                <p className="text-2xl">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit cupiditate asperiores perspiciatis culpa Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit cupiditate asperiores perspiciatis culpa voluptatum hic! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit cupiditate asperiores perspiciatis culpa voluptatum hic! voluptatum hic!
-                </p>
+            <div className='flex w-full md:w-[70%] justify-center mx-auto mt-20'>
+
+            <Accordion type="single" collapsible className="w-full">
+                <AccordionItem value="item-1">
+                    <AccordionTrigger className="text-2xl">Telefone</AccordionTrigger>
+                    <AccordionContent className=" font-bold">
+                    <p>+55 11 99999-9999</p>
+                    <p>+55 11 99999-9999</p>
+                    </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-2">
+                    <AccordionTrigger className="text-2xl">Instagram</AccordionTrigger>
+                    <AccordionContent className=" font-bold">
+                    <p>@adriankjk</p>
+                    <p>@adriankjk</p>
+                    <p>@adriankjk</p>
+                    </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-3">
+                    <AccordionTrigger className="text-2xl">Whatsapp</AccordionTrigger>
+                    <AccordionContent className=" font-bold">
+                    <p>+55 11 99999-9999</p>
+                    <p>+55 11 99999-9999</p>
+                    </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-4">
+                    <AccordionTrigger className="text-2xl">Facebook</AccordionTrigger>
+                    <AccordionContent className=" font-bold">
+                    <p>@adriankjk</p>
+                    <p>@adriankjk</p>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
             </div>
             
 
