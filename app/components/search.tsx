1a8c718ca7a679775e522e353570e181d6ddab4a
Adrian Graça <adrianspk1@gmail.com>
2024-09-24 20:29:03 -0300

    update

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -1,36 +1,36 @@
-'use client'
-import { useState } from 'react';
+// 'use client'
+// import { useState } from 'react';
 
-interface SearchProps {
-  handleSearch: (searchTerm: string) => void;
-}
+// interface SearchProps {
+//   handleSearch: (searchTerm: string) => void;
+// }
 
-function SearchBar({ handleSearch }: SearchProps) {
-  const [searchTerm, setSearchTerm] = useState('');
+// function SearchBar({ handleSearch }: SearchProps) {
+//   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log('form submitted')
-    handleSearch(searchTerm);
-  };
+//   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log('form submitted')
+//     handleSearch(searchTerm);
+//   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(event) => setSearchTerm(event.target.value)}
-        placeholder="Search..."
-        className="w-full p-2 pl-10 text-sm text-gray-700"
-      />
-      <button
-        type="submit"
-        className="bg-primary text-white p-2 rounded-lg"
-      >
-        Pesquisar
-      </button>
-    </form>
-  );
-}
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         value={searchTerm}
+//         onChange={(event) => setSearchTerm(event.target.value)}
+//         placeholder="Search..."
+//         className="w-full p-2 pl-10 text-sm text-gray-700"
+//       />
+//       <button
+//         type="submit"
+//         className="bg-primary text-white p-2 rounded-lg"
+//       >
+//         Pesquisar
+//       </button>
+//     </form>
+//   );
+// }
 
-export default SearchBar;
+// export default SearchBar;
--- app/components/searchManufacturer.tsx
+++ app/components/searchManufacturer.tsx
@@ -1,98 +1,98 @@
-"use client"
+// "use client"
 
-import * as React from "react"
-import { Car, Check, ChevronsUpDown, Factory } from "lucide-react"
-import { useState } from "react"
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-} from "@/components/ui/command"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover"
+// import * as React from "react"
+// import { Car, Check, ChevronsUpDown, Factory } from "lucide-react"
+// import { useState } from "react"
+// import { cn } from "@/lib/utils"
+// import { Button } from "@/components/ui/button"
+// import {
+//   Command,
+//   CommandEmpty,
+//   CommandGroup,
+//   CommandInput,
+//   CommandItem,
+//   CommandList,
+// } from "@/components/ui/command"
+// import {
+//   Popover,
+//   PopoverContent,
+//   PopoverTrigger,
+// } from "@/components/ui/popover"
 
-import { manufacturers } from "@/constants/constants"
-import { SearchBtn } from "./button"
+// import { manufacturers } from "@/constants/constants"
+// import { SearchBtn } from "./button"
 
-interface iAppProps {
-    manufacturer:string;
-    setManufacturer:(manufacturer: string) => void;
-}
+// interface iAppProps {
+//     manufacturer:string;
+//     setManufacturer:(manufacturer: string) => void;
+// }
 
-const frameworks = {manufacturers}
+// const frameworks = {manufacturers}
 
-export function SearchManufacturer({manufacturer, setManufacturer}: iAppProps) {
-  const [openf1, setOpenf1] = React.useState(false)
-  const [valuef1, setValuef1] = React.useState("")
+// export function SearchManufacturer({manufacturer, setManufacturer}: iAppProps) {
+//   const [openf1, setOpenf1] = React.useState(false)
+//   const [valuef1, setValuef1] = React.useState("")
 
-  const handleSelect = (value: string) => {
-    setValuef1(value)
-    setManufacturer(value)
-    setOpenf1(false)
-  }
+//   const handleSelect = (value: string) => {
+//     setValuef1(value)
+//     setManufacturer(value)
+//     setOpenf1(false)
+//   }
 
-  return (
+//   return (
     
-    <div className="md:flex">
-        <div className="mb-3 flex md:mr-3">
-        <Popover open={openf1} onOpenChange={setOpenf1}>
+//     <div className="md:flex">
+//         <div className="mb-3 flex md:mr-3">
+//         <Popover open={openf1} onOpenChange={setOpenf1}>
 
-          <PopoverTrigger asChild>
-          <Button
-              variant="secondary"
-              role="combobox"
-              aria-expanded={openf1}
-              className=" w-[300px] md:w-[350px] justify-between"
-          >
-              {valuef1
-              ? manufacturers.find((manufacturers) => manufacturers.value === valuef1)?.label
-              : <span className="flex items-center text-center"> <Factory className=" mr-2 text-muted-foreground"/> Selecione a montadora.</span> }
-              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+//           <PopoverTrigger asChild>
+//           <Button
+//               variant="secondary"
+//               role="combobox"
+//               aria-expanded={openf1}
+//               className=" w-[300px] md:w-[350px] justify-between"
+//           >
+//               {valuef1
+//               ? manufacturers.find((manufacturers) => manufacturers.value === valuef1)?.label
+//               : <span className="flex items-center text-center"> <Factory className=" mr-2 text-muted-foreground"/> Selecione a montadora.</span> }
+//               <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
 
-          </Button> 
+//           </Button> 
 
-          </PopoverTrigger>
+//           </PopoverTrigger>
 
-          <PopoverContent className="w-[200px] p-0">
-          <Command>
-              <CommandInput placeholder="Procurar montadora" />
-              <CommandList>
-              <CommandEmpty>Sem resultados.</CommandEmpty>
-              <CommandGroup>
-                  {manufacturers.map((manufacturers) => (
-                  <CommandItem
-                      key={manufacturers.value}
-                      value={manufacturers.value}
-                      onSelect={(currentValue) => {
-                        handleSelect(currentValue)}
-                      }
-                  >
-                      <Check
-                      className={cn(
-                          "mr-2 h-4 w-4",
-                          valuef1 === manufacturers.value ? "opacity-100" : "opacity-0"
-                      )}
-                      />
-                      {manufacturers.label}
-                  </CommandItem>
-                  ))}
-              </CommandGroup>
-              </CommandList>
-          </Command>
-          </PopoverContent>
+//           <PopoverContent className="w-[200px] p-0">
+//           <Command>
+//               <CommandInput placeholder="Procurar montadora" />
+//               <CommandList>
+//               <CommandEmpty>Sem resultados.</CommandEmpty>
+//               <CommandGroup>
+//                   {manufacturers.map((manufacturers) => (
+//                   <CommandItem
+//                       key={manufacturers.value}
+//                       value={manufacturers.value}
+//                       onSelect={(currentValue) => {
+//                         handleSelect(currentValue)}
+//                       }
+//                   >
+//                       <Check
+//                       className={cn(
+//                           "mr-2 h-4 w-4",
+//                           valuef1 === manufacturers.value ? "opacity-100" : "opacity-0"
+//                       )}
+//                       />
+//                       {manufacturers.label}
+//                   </CommandItem>
+//                   ))}
+//               </CommandGroup>
+//               </CommandList>
+//           </Command>
+//           </PopoverContent>
 
-        </Popover>
-        </div>
+//         </Popover>
+//         </div>
 
-    </div>
+//     </div>
     
-  )
-}
+//   )
+// }
