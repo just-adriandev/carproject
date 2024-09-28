0b73ed1f6c96af2bb7cd4eb59a45b26ce22f98a4
Adrian Graça <adrianspk1@gmail.com>
2024-09-28 13:38:44 -0300

    update
    auth improvements

---
+++ app/api/auth/creation/route.ts
@@ -0,0 +1,35 @@
+import prisma from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
+
+export async function GET(req: NextRequest) {
+  noStore();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user || user === null || !user.id)
+    throw new Error("something went wrong please try again");
+
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        email: user.email ?? "",
+        name: user.given_name ?? "",
+      },
+    });
+  }
+
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : "https://hivebra.vercel.app/"
+  );
+}
